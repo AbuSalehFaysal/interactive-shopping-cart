380bbd1ef4b2d043a2550c02c42071508bd31d71
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2021-01-27 02:28:51 +0600

    Clean and optimize the codes

--- public/js/script.js
+++ public/js/script.js
@@ -1,64 +1,73 @@
-let firstPlusButton = document.getElementById("first-plus-button");
-firstPlusButton.addEventListener("click", function () {
-    let mobileValue = document.getElementById("mobile-value").value;
+// ===================================
+// Net Price of Cart Products
+// ===================================
+const netMobilePrice = 1219;
+const netCoverPrice = 59;
+
+// =======================================================================
+// Function of Adding Product Quantity, Price and Update Total Price
+// =======================================================================
+function increaseValue(mobileValueParam, mobilePriceParam, coverPriceParam, subTotalParam, totalParam, netPrice) {
+    let mobileValue = document.getElementById(mobileValueParam).value;
     let mobileValueNum = parseFloat(mobileValue);
     let newMobileValue = mobileValueNum + 1;
     console.log(newMobileValue);
-    document.getElementById("mobile-value").value = newMobileValue;
-    let newMobilePriceNum = 1219 * newMobileValue;
-    document.getElementById("mobile-price").innerText = newMobilePriceNum;
-    let coverPrice = document.getElementById("cover-price").innerText;
+    document.getElementById(mobileValueParam).value = newMobileValue;
+    let newMobilePriceNum = netPrice * newMobileValue;
+    document.getElementById(mobilePriceParam).innerText = newMobilePriceNum;
+    let coverPrice = document.getElementById(coverPriceParam).innerText;
     let coverPriceNum = parseFloat(coverPrice);
-    document.getElementById("sub-total").innerText = newMobilePriceNum + coverPriceNum;
-    document.getElementById("total").innerText = newMobilePriceNum + coverPriceNum;
+    document.getElementById(subTotalParam).innerText = newMobilePriceNum + coverPriceNum;
+    document.getElementById(totalParam).innerText = newMobilePriceNum + coverPriceNum;
+}
 
-});
-
-let firstMinusButton = document.getElementById("first-minus-button");
-firstMinusButton.addEventListener("click", function () {
-    let mobileValue = document.getElementById("mobile-value").value;
+// =======================================================================
+// Function of Subtracting Product Quantity, Price and Update Total Price
+// =======================================================================
+function decreaseValue(mobileValueParam, mobilePriceParam, coverPriceParam, subTotalParam, totalParam, netPrice) {
+    let mobileValue = document.getElementById(mobileValueParam).value;
     let mobileValueNum = parseFloat(mobileValue);
     if (mobileValueNum > 0) {
         let newMobileValue = mobileValueNum - 1;
         console.log(newMobileValue);
-        document.getElementById("mobile-value").value = newMobileValue;
-        let newMobilePriceNum = 1219 * newMobileValue;
-        document.getElementById("mobile-price").innerText = newMobilePriceNum;
-        let coverPrice = document.getElementById("cover-price").innerText;
+        document.getElementById(mobileValueParam).value = newMobileValue;
+        let newMobilePriceNum = netPrice * newMobileValue;
+        document.getElementById(mobilePriceParam).innerText = newMobilePriceNum;
+        let coverPrice = document.getElementById(coverPriceParam).innerText;
         let coverPriceNum = parseFloat(coverPrice);
-        document.getElementById("sub-total").innerText = newMobilePriceNum + coverPriceNum;
-        document.getElementById("total").innerText = newMobilePriceNum + coverPriceNum;
+        document.getElementById(subTotalParam).innerText = newMobilePriceNum + coverPriceNum;
+        document.getElementById(totalParam).innerText = newMobilePriceNum + coverPriceNum;
     }
+}
+
+//========================================================
+// Adding Mobile Quantity, Price and Updating Total Price
+//========================================================
+let firstPlusButton = document.getElementById("first-plus-button");
+firstPlusButton.addEventListener("click", function () {
+    increaseValue("mobile-value", "mobile-price", "cover-price", "sub-total", "total", netMobilePrice)
 });
 
+//=============================================================
+// Subtracting Mobile Quantity, Price and Updating Total Price
+//=============================================================
+let firstMinusButton = document.getElementById("first-minus-button");
+firstMinusButton.addEventListener("click", function () {
+    decreaseValue("mobile-value", "mobile-price", "cover-price", "sub-total", "total", netMobilePrice);
+});
+
+//========================================================
+// Adding Cover Quantity, Price and Updating Total Price
+//========================================================
 let secondPlusButton = document.getElementById("second-plus-button");
 secondPlusButton.addEventListener("click", function () {
-    let coverValue = document.getElementById("cover-value").value;
-    let coverValueNum = parseFloat(coverValue);
-    let newCoverValue = coverValueNum + 1;
-    console.log(newCoverValue);
-    document.getElementById("cover-value").value = newCoverValue;
-    let newCoverPriceNum = 59 * newCoverValue;
-    document.getElementById("cover-price").innerText = newCoverPriceNum;
-    let mobilePrice = document.getElementById("mobile-price").innerText;
-    let mobilePriceNum = parseFloat(mobilePrice);
-    document.getElementById("sub-total").innerText = newCoverPriceNum + mobilePriceNum;
-    document.getElementById("total").innerText = newCoverPriceNum + mobilePriceNum;
+    increaseValue("cover-value", "cover-price", "mobile-price", "sub-total", "total", netCoverPrice)
 });
 
+//=============================================================
+// Subtracting Cover Quantity, Price and Updating Total Price
+//=============================================================
 let secondMinusButton = document.getElementById("second-minus-button");
 secondMinusButton.addEventListener("click", function () {
-    let coverValue = document.getElementById("cover-value").value;
-    let coverValueNum = parseFloat(coverValue);
-    if (coverValueNum > 0) {
-        let newCoverValue = coverValueNum - 1;
-        console.log(newCoverValue);
-        document.getElementById("cover-value").value = newCoverValue;
-        let newCoverPriceNum = 59 * newCoverValue;
-        document.getElementById("cover-price").innerText = newCoverPriceNum;
-        let mobilePrice = document.getElementById("mobile-price").innerText;
-        let mobilePriceNum = parseFloat(mobilePrice);
-        document.getElementById("sub-total").innerText = newCoverPriceNum + mobilePriceNum;
-        document.getElementById("total").innerText = newCoverPriceNum + mobilePriceNum;
-    }
+    decreaseValue("cover-value", "cover-price", "mobile-price", "sub-total", "total", netCoverPrice);
 });
