7bcda6fc63db6af02a3a1b6c3b9fcaff752b59ee
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2021-01-27 00:05:06 +0600

    Plus Minus Fuctionality of Mobile Phone

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,19 @@
-alert("JavaScript Added!");
+let firstPlusButton = document.getElementById("first-plus-button");
+firstPlusButton.addEventListener("click", function () {
+    let mobileValue = document.getElementById("mobile-value").value;
+    let mobileValueNum = parseFloat(mobileValue);
+    let newMobileValue = mobileValueNum + 1;
+    console.log(newMobileValue);
+    document.getElementById("mobile-value").value = newMobileValue;
+});
+
+let firstMinusButton = document.getElementById("first-minus-button");
+firstMinusButton.addEventListener("click", function () {
+    let mobileValue = document.getElementById("mobile-value").value;
+    let mobileValueNum = parseFloat(mobileValue);
+    if (mobileValueNum > 0) {
+        let newMobileValue = mobileValueNum - 1;
+        console.log(newMobileValue);
+        document.getElementById("mobile-value").value = newMobileValue;
+    }
+});
